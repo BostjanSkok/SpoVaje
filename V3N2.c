c4f5e88146eba0d26e9cc942d3e0638fda1d903b
BostjanSkok <BostjanSkok2@gmail.com>
2013-10-29 21:25:24 +0100

    V3N2 finished

---
+++ V3N2.c
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
+void WirteFileType(mode_t toConvert)
+{
+    if (S_ISDIR(toConvert))
+        printf("d");
+    /*else if(S_ISLNK(toConvert))
+        printf("l");*/
+    else if (S_ISCHR(toConvert))
+        printf("c");
+    else
+        printf("-");
+
+}
+void WriteFilePerm(mode_t toConvert)
+{
+    printf( (toConvert & S_IRUSR) ? "r" : "-");
+    printf( (toConvert & S_IWUSR) ? "w" : "-");
+    printf( (toConvert & S_IXUSR) ? "x" : "-");
+  /*  printf( (toConvert & S_IRGRP) ? "r" : "-");
+    printf( (toConvert & S_IWGRP) ? "w" : "-");
+    printf( (toConvert & S_IXGRP) ? "x" : "-");
+    printf( (toConvert & S_IROTH) ? "r" : "-");
+    printf( (toConvert & S_IWOTH) ? "w" : "-");
+    printf( (toConvert & S_IXOTH) ? "x" : "-");*/
+
+    printf(" ");
+}
+
+int main(int argc, char **argv)
+{
+    DIR *dp ;
+    if ((dp = opendir(".")) == NULL)
+    {
+        printf("Can't open %s", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    struct dirent *dirp;
+    struct stat *buf = malloc(sizeof(struct stat));;
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        stat( dirp->d_name, buf);
+
+        WirteFileType(buf->st_mode);
+        WriteFilePerm(buf->st_mode);
+       // printf("%s ",buf->st_nlink);
+        printf("%s\n", dirp->d_name);
+    }
+
+    closedir(dp);
+    exit(EXIT_SUCCESS);
+}
